f1352138af967183e58fb85154c17ed6a171e359
Sri Sandeep <sandyorton7@gmail.com>
2025-08-21 01:09:20 +0100

    Challenge 1

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println(" SSS    SSS    SSS ");
+        System.out.println("S   S  S   S  S   S");
+        System.out.println("S      S      S    ");
+        System.out.println(" SSS    SSS    SSS ");
+        System.out.println("    S      S      S");
+        System.out.println("S   S  S   S  S   S");
+        System.out.println(" SSS    SSS    SSS ");
+
     }
 }
