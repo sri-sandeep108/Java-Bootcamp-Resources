4abe592cf92b28d24c649ce729678b717ec4a910
Sri Sandeep <sandyorton7@gmail.com>
2025-08-22 12:43:56 +0100

    Workbook 3.2

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -8,20 +8,20 @@ public class JavaMart {
         double toyCar = 5.99;
         System.out.println("Can I get this car?");
        
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the toy car 
+       if (wallet >= toyCar){
+            System.out.println("Sure");
+            wallet -= toyCar; }
+        else
+            System.out.println("Sorry, I have only $"+wallet+" left");
 
-        //else:  Sorry, I only have <wallet> left.
 
         
         double nike = 95.99;
         System.out.println("Can I get these nike shoes?");
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the nike shoes 
-
-        //else: Sorry, I only have <wallet> left.
-
+        if (wallet >= nike){
+            System.out.println("Sure");
+            wallet -= nike; }
+        else
+            System.out.println("Sorry, I have only $"+wallet+" left");
     }
 }
