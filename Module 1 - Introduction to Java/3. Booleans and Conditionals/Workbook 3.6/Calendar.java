afdb74d66d06ffd34a78257aa2e7efd9dd712e18
Sri Sandeep <sandyorton7@gmail.com>
2025-08-22 13:42:36 +0100

    Workbook 3.6

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
@@ -9,7 +9,16 @@ public class Calendar {
 
         System.out.println("Hmm, let me check my calendar.");
         
-        //Check 'calendar' here:        
+        switch (day) {
+            case "Monday": System.out.println("Sorry, I have to stay at work late.");
+            case "Tuesday": System.out.println("It looks like I have meetings all day.");
+            case "Wednesday": System.out.println("I have a dentist appointment. Some other time!");
+            case "Thursday": System.out.println("Sorry, thursday is date night!");
+            case "Friday": System.out.println("I'm free!!");
+            case "Saturday": System.out.println("I'm free!!");
+            case "Sunday": System.out.println("I'm free!!");
+            default : System.out.println("That's not a day");
+        }  
         
 
     }
