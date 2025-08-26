36717ec45e092f61b0f686f82d699f88d33a25bb
Sri Sandeep <sandyorton7@gmail.com>
2025-08-27 00:39:55 +0100

    Workbook 5.11

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
@@ -7,10 +7,43 @@ public class JavaRolls {
         
         int diceRoll;
         int score = 0;
+        System.out.println("Let's play Rolling Java. Type anything to start.");
+        scan.nextLine();
+        System.out.println("Great, here are the rules:\n");
+        System.out.println("- If you roll a 6, the game stops.");
+        System.out.println("- If you roll a 4, nothing happens.");
+        System.out.println("- Otherwise, you get 1 point.\n");
+        System.out.println("You must collect at least 3 points to win. Enter anything to roll:");
+        scan.nextLine();
 
+        while (true) {
+            diceRoll = rollDice();
+            System.out.print("You rolled a " + diceRoll + ". ");
+            if (diceRoll == 6) {
+                System.out.println("End of Game");
+                break;
+            }
+            else if (diceRoll == 3) {
+                System.out.println("Zero Points. Keep rolling");
+            }
+            else {
+                score += 1;
+                System.out.println("One point. Keep rolling");
+            }
+        }
+
+        System.out.println("Your score is: " + score);
+        if (score >=3){
+            System.out.println("Wow, that's lucky. You win!");
+        }
+        else {
+            System.out.println("Tough luck, you lose :(");
+        }
     }
   
-  
+    public static int rollDice(){
+        return (int) (Math.random() * (6-1+1) +1);
+    }
 
   
   
