d40f93b978914822d5bc1c126a3e0dfe00d726da
Sri Sandeep <sandyorton7@gmail.com>
2025-08-27 22:17:13 +0100

    Workbook 6.5

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
@@ -1,14 +1,30 @@
 public class HighScore {
     public static void main(String[] args) {
-        
+
         int highScore = 0;
-        
+        int[] scores = new int[10];
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = randomNumberGenerator();
+        }
+
         // Instructions for this workbook are on Learn the Part (Workbook 6.5).
 
-        System.out.print("Here are the scores: <score elements>");
+        System.out.print("Here are the scores: ");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + " ");
+        }
+        for (int i = 0; i < scores.length; i++) {
+            if (highScore < scores[i]) {
+                highScore = scores[i];
+            }
+        }
 
         System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
-        
-    }    
+
+    }
+
+    public static int randomNumberGenerator() {
+        return (int) (Math.random() * (49999 - 0 + 1) + 0);
+    }
 
 }
