edab35cc97320c5e594eeb203d2747f000e20818
Sri Sandeep <sandyorton7@gmail.com>
2025-08-28 00:55:57 +0100

    Workbook 6.11

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.11/Weather.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.11/Weather.java
@@ -1,11 +1,33 @@
+import java.util.Arrays;
+
 public class Weather {
     public static void main(String[] args) {
-        
-        // The instructions for this workbook are on Learn the Part (Workbook 6.11)
-        double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
-        double[] fahrenheit;
-        
 
+        // The instructions for this workbook are on Learn the Part (Workbook 6.11)
+        double[] celsius = { 12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0 };
+        double[] fahrenheit = celsiusToFarenhit(celsius);
+
+        printTemperatures(celsius, "Celsius");
+        printTemperatures(fahrenheit, "Farenheit");
+
+    }
+
+    public static double[] celsiusToFarenhit(double[] celsius) {
+        double[] fahrenheit = new double[celsius.length];
+
+        for (int i = 0; i < celsius.length; i++) {
+            fahrenheit[i] = (celsius[i] / 5 * 9) + 32;
+        }
+
+        return fahrenheit;
+    }
+
+    public static void printTemperatures(double[] temps, String type) {
+        System.out.print(type + ": ");
+        for (int i = 0; i < temps.length; i++) {
+            System.out.print(temps[i] + " ");
+        }
+        System.out.println("");
     }
 
 }
