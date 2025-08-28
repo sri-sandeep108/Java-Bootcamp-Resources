d63bcf3f2c679b7a05ac0659c1b2f318e1e14ee7
Sri Sandeep <sandyorton7@gmail.com>
2025-08-28 01:48:09 +0100

    Workbook 6.13

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,5 +1,16 @@
 public class Prices {
     public static void main(String[] args) {
-        // The instructions for this workbook are on Learn the Part (workbook 6.13)
+        double[][] prices = {
+                { 12.99, 8.99, 9.99, 10.49, 11.99 },
+                { 0.99, 1.99, 2.49, 1.49, 2.99 },
+                { 8.99, 7.99, 9.49, 9.99, 10.99 }
+        };
+
+        System.out.println("Baking : " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3]
+                + " " + prices[0][4]);
+        System.out.println("Beverage : " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3]
+                + " " + prices[0][4]);
+        System.out.println("Cereals : " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3]
+                + " " + prices[0][4]);
     }
 }
