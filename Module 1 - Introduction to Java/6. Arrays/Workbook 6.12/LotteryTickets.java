d8e183372edcd894a64abea040e99079ea434254
Sri Sandeep <sandyorton7@gmail.com>
2025-08-28 01:07:43 +0100

    Workbook 6.12

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.12/LotteryTickets.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.12/LotteryTickets.java
@@ -2,17 +2,23 @@ import java.util.Arrays;
 
 public class LotteryTickets {
     public static void main(String[] args) {
-        
+
         // Instructions for this workbook are on Learn the Part (workbook 6.12)
-        int[] ticket = {34, 43, 45, 65, 21, 54};
-        int[] ticket2;
+        int[] ticket = { 34, 43, 45, 65, 21, 54 };
+        int[] ticket2 = Arrays.copyOf(ticket, ticket.length);
 
         System.out.print("Ticket 1 numbers: ");
-        //Task 3 - Call printTicketNumbers for int[] ticket.   
+        printTicketNumbers(ticket); // Task 3 - Call printTicketNumbers for int[] ticket.
 
         System.out.print("Ticket 2 numbers: ");
-        //Task 3 - Call printTicketNumbers for int[] ticket2.   
-    } 
-    
-}
+        printTicketNumbers(ticket); // Task 3 - Call printTicketNumbers for int[] ticket2.
+    }
 
+    public static void printTicketNumbers(int[] ticket) {
+        for (int i = 0; i < ticket.length; i++) {
+            System.out.print(ticket[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+}
