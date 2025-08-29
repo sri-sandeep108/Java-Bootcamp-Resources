50485b244bbdce9d721969423de6ea7c08e7b35b
Sri Sandeep <sandyorton7@gmail.com>
2025-08-29 11:20:16 +0100

    Workbook 7.2

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.2/Person.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.2/Person.java
@@ -1,9 +1,16 @@
 public class Person {
-    
+
     String name;
     String nationality;
     String dateOfBirth;
     String[] passport;
     int seatNumber;
- 
+
+    Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
 }
