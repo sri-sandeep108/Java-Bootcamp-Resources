250a986c8526f5c12479cb4dec6b71441f4828bf
Sri Sandeep <sandyorton7@gmail.com>
2025-08-29 11:13:35 +0100

    Workbook 7.1

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -1,3 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        System.out.println(person.name);
+        System.out.println(person.nationality);
+        System.out.println(person.dateOfBirth);
+        System.out.println(Arrays.toString(person.passport));
+        System.out.println(person.seatNumber);
+
+        person.name = "Sam Billings";
+        person.nationality = "England";
+        person.dateOfBirth = "10/10/2010";
+        person.passport = new String[] { "H", "A", "D", "P", "1", "2", "3", "4" };
+        person.seatNumber = 1;
+
+        System.out.println(person.name);
+        System.out.println(person.nationality);
+        System.out.println(person.dateOfBirth);
+        System.out.println(Arrays.toString(person.passport));
+        System.out.println(person.seatNumber);
+    }
 }
---
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    public String name;
+    public String nationality;
+    public String dateOfBirth;
+    public String[] passport;
+    public int seatNumber;
+}
