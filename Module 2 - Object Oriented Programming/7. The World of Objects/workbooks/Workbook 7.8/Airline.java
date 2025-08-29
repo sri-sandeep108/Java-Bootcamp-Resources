34d2b8ad1a7f2840ac6eab572763da36f3d04fd2
Sri Sandeep <sandyorton7@gmail.com>
2025-08-30 00:07:19 +0100

    Workbook 7.8

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.8/Airline.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.8/Airline.java
@@ -1,6 +1,18 @@
 public class Airline {
-    
-    Person[] people; //array that stores Person objects...
 
+    Person[] people; // array that stores Person objects...
+
+    public Airline() {
+        people = new Person[11];
+    }
+
+    public Person getPerson(int index) {
+        Person copy = new Person(people[index]);
+        return copy;
+    }
+
+    public void setPerson(Person source) {
+        people[source.getSeatNumber() - 1] = new Person(source);
+    }
 
 }
