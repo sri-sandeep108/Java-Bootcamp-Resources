a58c817dd541a936b8196787f49a7163de9ce898
Sri Sandeep <sandyorton7@gmail.com>
2025-09-04 13:47:56 +0100

    Workbook 9.1

--- Module 2 - Object Oriented Programming/11. Map Collections/Workbook/starter/Team.java
+++ Module 2 - Object Oriented Programming/11. Map Collections/Workbook/starter/Team.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Team {
@@ -6,26 +7,24 @@ public class Team {
     private Map<String, String> players;
 
     public Team(String name) {
-        // TODO
+        setName(name);
+        players = new HashMap<>();
     }
-    
+
     public String getName() {
-        // TODO
-        return null;
+        return name;
     }
 
     public void setName(String name) {
-        // TODO
+        this.name = name;
     }
 
     public String getPlayer(String position) {
-        // TODO
-        return null;
+        return players.get(position);
     }
 
     public void setPlayer(String position, String player) {
-        // TODO
-    }    
+        players.put(position, player);
+    }
 
-    
 }
