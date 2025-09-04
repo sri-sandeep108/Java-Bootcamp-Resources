a0c762c4b56fc349101af016490620af9454b7eb
Sri Sandeep <sandyorton7@gmail.com>
2025-09-04 14:33:09 +0100

    Extra Practice Part 3

--- Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 3/Main.java
+++ Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 3/Main.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String cityName = promptForCityName(scanner);
+
+        String country = promptForCountry(scanner);
+
+        int population = promptForPopulation(scanner);
+
+        City newCity = new City(cityName, country, population);
+
+        CityPopulationTracker tracker = new CityPopulationTracker();
+        tracker.addCity(newCity);
+
+        System.out.println("City added to the tracker: " + newCity.getName());
     }
+
+    public static boolean isNullorBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    public static boolean incorrectPopulation(int population) {
+        return population <= 0;
+    }
+
+    public static String promptForCityName(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid city name: ");
+            String cityName = scanner.nextLine();
+            if (isNullorBlank(cityName))
+                continue;
+            return cityName;
+        }
+    }
+
+    public static String promptForCountry(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid country: ");
+            String country = scanner.nextLine();
+            if (isNullorBlank(country))
+                continue;
+            return country;
+        }
+    }
+
+    public static int promptForPopulation(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid population (greater than 0): ");
+            if (!scanner.hasNextInt()) {
+                scanner.next();
+                continue;
+            }
+            int population = scanner.nextInt();
+            if (incorrectPopulation(population))
+                continue;
+            return population;
+        }
+    }
+
 }
