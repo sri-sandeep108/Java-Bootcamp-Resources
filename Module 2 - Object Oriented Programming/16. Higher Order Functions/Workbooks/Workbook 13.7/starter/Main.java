7edc1ec840f35d5318268cd94bdc9c9207270736
Sri Sandeep <sandyorton7@gmail.com>
2025-09-05 17:26:40 +0100

    Workbook 13.7

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Workbooks/Workbook 13.7/starter/Main.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Workbooks/Workbook 13.7/starter/Main.java
@@ -1,10 +1,16 @@
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("emails.txt");
-        
+        try {
+            Files.lines(path).filter(line -> !line.startsWith("Spam:"))
+                    .forEach(line -> System.out.println(line));
+        } catch (IOException e) {
+            System.out.println("The file is not found!");
+        }
     }
 }
