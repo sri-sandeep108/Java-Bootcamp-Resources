0e32e387da40c18bccf98a8f25ce000276ec3741
Sri Sandeep <sandyorton7@gmail.com>
2025-09-05 18:02:17 +0100

    Extra Practice Part 1

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/Movie.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/Movie.java
@@ -1,5 +1,72 @@
 package model;
 
 public class Movie {
+    private String title;
+    private String director;
+    private String genre;
+    private int year;
+    private double rating;
+
+    public Movie(String title, String director, String genre, int year, double rating) {
+        setTitle(title);
+        setDirector(director);
+        setGenre(genre);
+        setYear(year);
+        setRating(rating);
+    }
+
+    public Movie(Movie source) {
+        setTitle(source.getTitle());
+        setDirector(source.getDirector());
+        setGenre(source.getGenre());
+        setYear(source.getYear());
+        setRating(source.getRating());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String toString() {
+        return getTitle() + " - " + getDirector() + " - " + getGenre() + " - "
+                + getYear()
+                + " - " + getRating();
+    }
 
 }
--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/MovieStore.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/MovieStore.java
@@ -1,5 +1,35 @@
 package model;
 
-public class MovieStore {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MovieStore {
+    private List<Movie> movies;
+
+    public MovieStore() {
+        this.movies = new ArrayList<>();
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(new Movie(movie));
+    }
+
+    public List<Movie> filterByGenre(String genre) {
+        return movies.stream()
+                .filter(movie -> movie.getGenre().equals(genre))
+                .toList();
+    }
+
+    public List<Movie> sortByReleaseYear() {
+        return movies.stream()
+                .sorted((x, y) -> Integer.compare(x.getYear(), y.getYear()))
+                .toList();
+    }
+
+    public List<Movie> getTopRatedMovies(int n) {
+        return movies.stream()
+                .sorted((x, y) -> Double.compare(y.getRating(), x.getRating()))
+                .limit(n)
+                .toList();
+    }
 }
