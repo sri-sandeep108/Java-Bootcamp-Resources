e5cf6d7d107f66f498032a0ee68500e8b1cbd5f9
Sri Sandeep <sandyorton7@gmail.com>
2025-09-05 15:25:26 +0100

    Workbook 13.2

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Workbooks/Workbook 13.2/starter/Main.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Workbooks/Workbook 13.2/starter/Main.java
@@ -6,30 +6,29 @@ public class Main {
 
         List<Double> prices = Arrays.asList(55.99, 33.99, 88.99, 11.49, 55.99, 111.99, 77.99);
 
-        // TODO
+        prices.sort((a, b) -> a.compareTo(b));
 
         System.out.println("\nPrices\n------");
 
-        // TODO
+        prices.forEach(price -> System.out.print(price + " "));
 
         List<String> books = Arrays.asList(
-            "To Kill a Mockingbird",
-            "The Great Gatsby",
-            "Pride and Prejudice",
-            "The Catcher in the Rye",
-            "The Alchemist",
-            "One Hundred Years of Solitude",
-            "Moby-Dick",
-            "The Brothers Karamazov",
-            "The Lord of the Rings",
-            "The Picture of Dorian Gray"
-        );
+                "To Kill a Mockingbird",
+                "The Great Gatsby",
+                "Pride and Prejudice",
+                "The Catcher in the Rye",
+                "The Alchemist",
+                "One Hundred Years of Solitude",
+                "Moby-Dick",
+                "The Brothers Karamazov",
+                "The Lord of the Rings",
+                "The Picture of Dorian Gray");
 
-        // TODO
+        books.sort((a, b) -> a.compareTo(b));
 
         System.out.println("\nLibrary\n--------");
 
-        // TODO
+        books.forEach(book -> System.out.println(book));
 
     }
 }
