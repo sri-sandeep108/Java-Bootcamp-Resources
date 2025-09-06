9bece2ef89602e99b1ebc05a7f58edc6d18ef1cd
Sri Sandeep <sandyorton7@gmail.com>
2025-09-06 22:56:10 +0100

    Capstone Part 1

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/CashAccount.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/CashAccount.java
@@ -1,6 +1,26 @@
 package pojo;
 
-public class CashAccount {
+import java.math.BigDecimal;
 
+public class CashAccount extends TradeAccount {
+    private BigDecimal cashBalance;
+
+    public CashAccount(String id, BigDecimal decimal) {
+        super(id);
+        this.cashBalance = new BigDecimal(decimal.toString());
+    }
+
+    public BigDecimal getCashBalance() {
+        return cashBalance;
+    }
+
+    public void setCashBalance(BigDecimal cashBalance) {
+        this.cashBalance = cashBalance;
+    }
+
+    @Override
+    public CashAccount clone() {
+        return new CashAccount(super.getId(), cashBalance);
+    }
 
 }
--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/MarginAccount.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/MarginAccount.java
@@ -1,6 +1,26 @@
 package pojo;
 
-public class MarginAccount  {
+import java.math.BigDecimal;
 
+public class MarginAccount extends TradeAccount {
+    private BigDecimal margin;
+
+    public MarginAccount(String id, BigDecimal decimal) {
+        super(id);
+        margin = new BigDecimal(decimal.toString());
+    }
+
+    public BigDecimal getMargin() {
+        return margin;
+    }
+
+    public void setMargin(BigDecimal margin) {
+        this.margin = margin;
+    }
+
+    @Override
+    public MarginAccount clone() {
+        return new MarginAccount(super.getId(), margin);
+    }
 
 }
