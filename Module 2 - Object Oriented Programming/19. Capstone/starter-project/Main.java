06b901d569d65d9c233779aadb465dd518eda90b
Sri Sandeep <sandyorton7@gmail.com>
2025-09-06 23:03:43 +0100

    Capstone Part 2

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/Main.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/Main.java
@@ -2,29 +2,25 @@ import java.math.BigDecimal;
 
 import pojo.CashAccount;
 import pojo.MarginAccount;
+import pojo.TradeAccount;
+import repository.TradeAccountRepository;
 
 public class Main {
     public static void main(String[] args) {
+        TradeAccountRepository repository = new TradeAccountRepository();
+
         CashAccount cashAccount = new CashAccount("C123", new BigDecimal("1000.00"));
-        System.out.println("Cash Account Details:");
-        System.out.println("ID: " + cashAccount.getId());
-        System.out.println("Cash Balance: " + cashAccount.getCashBalance());
-
-        CashAccount clonedCashAccount = (CashAccount) cashAccount.clone();
-        System.out.println("Cloned Cash Account Details:");
-        System.out.println("ID: " + clonedCashAccount.getId());
-        System.out.println("Cash Balance: " + clonedCashAccount.getCashBalance());
-
-        System.out.println();
+        repository.createTradeAccount(cashAccount);
 
         MarginAccount marginAccount = new MarginAccount("M456", new BigDecimal("5000.00"));
-        System.out.println("Margin Account Details:");
-        System.out.println("ID: " + marginAccount.getId());
-        System.out.println("Margin: " + marginAccount.getMargin());
+        repository.createTradeAccount(marginAccount);
 
-        MarginAccount clonedMarginAccount = (MarginAccount) marginAccount.clone();
-        System.out.println("Cloned Margin Account Details:");
-        System.out.println("ID: " + clonedMarginAccount.getId());
-        System.out.println("Margin: " + clonedMarginAccount.getMargin());
+        TradeAccount retrievedCashAccount = repository.retrieveTradeAccount("C123");
+        System.out.println("Retrieved Cash Account ID: " + retrievedCashAccount.getId());
+        System.out.println("Cash Balance: " + ((CashAccount) retrievedCashAccount).getCashBalance());
+
+        TradeAccount retrievedMarginAccount = repository.retrieveTradeAccount("M456");
+        System.out.println("Retrieved Margin Account ID: " + retrievedMarginAccount.getId());
+        System.out.println("Margin: " + ((MarginAccount) retrievedMarginAccount).getMargin());  
     }
 }
--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/repository/TradeAccountRepository.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/repository/TradeAccountRepository.java
@@ -1,7 +1,26 @@
 package repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import pojo.TradeAccount;
 
 public class TradeAccountRepository {
+    private Map<String, TradeAccount> datastore = new HashMap<>();
 
+    public void createTradeAccount(TradeAccount source) {
+        datastore.put(source.getId(), source.clone());
+    }
 
+    public TradeAccount retrieveTradeAccount(String id) {
+        return this.datastore.get(id) == null ? null : this.datastore.get(id).clone();
+    }
+
+    public void updateTradeAccount(TradeAccount source) {
+        datastore.put(source.getId(), source.clone());
+    }
+
+    public void deleteTradeAccount(String id) {
+        datastore.remove(id);
+    }
 }
