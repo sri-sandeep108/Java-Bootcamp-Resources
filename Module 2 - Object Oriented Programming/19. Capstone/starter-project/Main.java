9036daa698e0745dd062c9dc2900845c54b25dc2
Sri Sandeep <sandyorton7@gmail.com>
2025-09-07 22:42:51 +0100

    Capstone Part 4

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/Main.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/Main.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import pojo.CashAccount;
 import pojo.MarginAccount;
@@ -7,33 +11,63 @@ import service.CashAccountService;
 import service.MarginAccountService;
 
 public class Main {
+    static Path[] paths = new Path[] { Paths.get("data/accounts.txt"), Paths.get("data/transactions.txt") };
+
+    static TradeAccountRepository tradeAccountRepository = new TradeAccountRepository();
+    static CashAccountService cashAccountService = new CashAccountService(tradeAccountRepository);
+    static MarginAccountService marginAccountService = new MarginAccountService(tradeAccountRepository);
+
     public static void main(String[] args) {
-        TradeAccountRepository repository = new TradeAccountRepository();
-        CashAccountService cashAccountService = new CashAccountService(repository);
-        MarginAccountService marginAccountService = new MarginAccountService(repository);
+        try {
+            loadTradeAccounts();
+            applyTransactions();
+            finalTest();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
 
-        // Create CashAccount and MarginAccount objects
-        CashAccount cashAccount = new CashAccount("1", BigDecimal.valueOf(1000));
-        MarginAccount marginAccount = new MarginAccount("2", BigDecimal.valueOf(2000));
+    }
 
-        // Add the accounts to the repository
-        cashAccountService.createTradeAccount(cashAccount);
-        marginAccountService.createTradeAccount(marginAccount);
+    public static void loadTradeAccounts() throws IOException {
+        Files.lines(paths[0])
+                .map(x -> x.split(" "))
+                .forEach(line -> {
+                    if (line[0].equals("CASH")) {
+                        CashAccount temp = new CashAccount(line[1], new BigDecimal(line[2]));
+                        cashAccountService.createTradeAccount(temp);
+                    } else if (line[0].equals("MARGIN")) {
+                        MarginAccount temp = new MarginAccount(line[1], new BigDecimal(line[2]));
+                        marginAccountService.createTradeAccount(temp);
+                    }
+                });
+    }   
 
-        // Deposit and withdraw amounts
-        cashAccountService.deposit("1", BigDecimal.valueOf(500));
-        cashAccountService.withdraw("1", BigDecimal.valueOf(200));
-        marginAccountService.deposit("2", BigDecimal.valueOf(1000));
-        marginAccountService.withdraw("2", BigDecimal.valueOf(500));
+    public static void applyTransactions() throws IOException {
+        Files.lines(paths[1])
+                .map(x -> x.split(" "))
+                .forEach(line -> {
+                    if (line[0].equals("CASH")) {
+                        if (line[2].equals("DEPOSIT"))
+                            cashAccountService.deposit(line[1], new BigDecimal(line[3]));
+                        else
+                            cashAccountService.withdraw(line[1], new BigDecimal(line[3]));
+                    } else if (line[0].equals("MARGIN")) {
+                        if (line[2].equals("DEPOSIT"))
+                            marginAccountService.deposit(line[1], new BigDecimal(line[3]));
+                        else
+                            marginAccountService.withdraw(line[1], new BigDecimal(line[3]));
+                    }
+                });
+    }
 
-        // Retrieve and print the updated account balances
-        CashAccount updatedCashAccount = cashAccountService.retrieveTradeAccount("1");
-        MarginAccount updatedMarginAccount = marginAccountService.retrieveTradeAccount("2");
-        System.out.println("Updated CashAccount balance: " + updatedCashAccount.getCashBalance());
-        System.out.println("Updated MarginAccount margin: " + updatedMarginAccount.getMargin());
-
-        // Delete the accounts
-        cashAccountService.deleteTradeAccount("1");
-        marginAccountService.deleteTradeAccount("2");
+    public static void finalTest() throws IOException {
+        System.out.println(
+                "Account A1234B Cash Balance: " + cashAccountService.retrieveTradeAccount("A1234B").getCashBalance());
+        System.out.println(
+                "Account E3456F Cash Balance: " + cashAccountService.retrieveTradeAccount("E3456F").getCashBalance());
+        System.out.println(
+                "Account I5678J Cash Balance: " + cashAccountService.retrieveTradeAccount("I5678J").getCashBalance());
+        System.out.println("Account C2345D Margin: " + marginAccountService.retrieveTradeAccount("C2345D").getMargin());
+        System.out.println("Account G4567H Margin: " + marginAccountService.retrieveTradeAccount("G4567H").getMargin());
     }
 }
